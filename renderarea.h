046b546798a1f31346faff5047ff926ab802ff4f
EDash Admin <github@edash.tech>
2018-04-07 10:32:53 -0500

    Added dynamic curves

--- renderarea.h
+++ renderarea.h
@@ -11,12 +11,12 @@ public:
     QSize minimumSizeHint() const Q_DECL_OVERRIDE;
     QSize sizeHint() const Q_DECL_OVERRIDE;
     enum ShapeType{
-        Astroid,Cycloid,HuygensCycloid,HypoCycloid
+        Astroid,Cycloid,HuygensCycloid,HypoCycloid,FutureCurve
     };
     void setBackgroundColor(QColor color){mBackgroundColor = color;} //setter
     QColor backgroundColor()const {return mBackgroundColor;}
 
-    void setShape(ShapeType shape){mShape = shape;}
+    void setShape(ShapeType shape){mShape = shape; on_shape_changed();}
     ShapeType shape() const {return mShape;}
 protected:
     void paintEvent(QPaintEvent *event) Q_DECL_OVERRIDE;
@@ -25,11 +25,20 @@ signals:
 
 public slots:
 private:
+    void on_shape_changed();
    QPointF compute_astroid(float t);
+   QPointF compute(float t);
+   QPointF compute_cycloid(float t);
+   QPointF compute_huygens(float t);
+   QPointF compute_hypo(float t);
+   QPointF compute_future_curve(float t);
 private:
     QColor mBackgroundColor;
     QColor mShapeColor;
     ShapeType mShape;
+    float mIntervalLength;
+    float mScale;
+    float mStepCount;
 };
 
 #endif // RENDERAREA_H
